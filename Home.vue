9534549a7694693e5c46264da96f15d4ced4fba9
zxzp <48046242+zxzp@users.noreply.github.com>
2019-03-12 09:53:19 +0800

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    {{msg}}
+    <hr>
+    {{msg1}}
+    <hr>
+    <son :pop="popup"></son>
+  </div>
+
+</template>
+
+<script>
+  import Son from './son.vue';
+    export default {
+        name: "Home",
+        data(){
+          return{
+            msg:"这是home组件里定义的msg"
+          }
+        },
+       components:{
+          'son':Son
+       },
+      methods:{
+          popup(val){
+            alert("这是home组件的popup方法"+val)
+          }
+      },
+        props:['msg1']
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ son.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <button @click="pop(123)">sonbutton</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "son",
+      data(){
+          return{
+            title:'this is son page'
+          }
+      },
+      props:['pop']
+    }
+</script>
+
+<style scoped>
+
+</style>
